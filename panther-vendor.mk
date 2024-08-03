1c5ad797f42f095433471ee0de098ae11bb155cd
Jackeagle <jackeagle102@gmail.com>
2024-08-03 09:04:31 -0400

    panther: Drop DeviceIntelligenceNetwork, DevicePersonalization, HbmSVManager
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- panther-vendor.mk
+++ panther-vendor.mk
@@ -84,8 +84,6 @@ PRODUCT_PACKAGES += \
     ConnMO \
     DCMO \
     DMService \
-    DeviceIntelligenceNetworkPrebuilt-v.U.14.playstore \
-    DevicePersonalizationPrebuiltPixel2022-v.U.14.playstore \
     DiagMon \
     EuiccGoogle \
     HardwareInfo \
@@ -101,7 +99,6 @@ PRODUCT_PACKAGES += \
     EuiccGoogleOverlay \
     EuiccSupportPixel \
     EuiccSupportPixelPermissions \
-    HbmSVManager \
     MyVerizonServices \
     OemRilService \
     PixelQualifiedNetworksService \
